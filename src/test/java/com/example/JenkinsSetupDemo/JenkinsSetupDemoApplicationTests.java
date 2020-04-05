75d5c8e0e27052ccefb1583cad83806fc4ba0e24
Kailash <kailashsaini7@gmail.com>
2020-04-05 23:44:21 +0530

    kailash commit

--- src/test/java/com/example/JenkinsSetupDemo/JenkinsSetupDemoApplicationTests.java
+++ src/test/java/com/example/JenkinsSetupDemo/JenkinsSetupDemoApplicationTests.java
@@ -16,7 +16,7 @@ class JenkinsSetupDemoApplicationTests {
 	Logger logger = LoggerFactory.getLogger(JenkinsSetupDemoApplicationTests.class);
 
 	@Test
-	public  contextLoads() {
+	public void contextLoads() {
 		logger.info("Test case execution started kailash.....................");
 
 		Assert.assertEquals(true, true);
