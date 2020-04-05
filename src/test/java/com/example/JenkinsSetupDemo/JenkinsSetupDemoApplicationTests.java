27697fcc689f558db5d74e10b66d43123519a9cf
Kailash <kailashsaini7@gmail.com>
2020-04-05 16:47:38 +0530

    third commit

--- src/test/java/com/example/JenkinsSetupDemo/JenkinsSetupDemoApplicationTests.java
+++ src/test/java/com/example/JenkinsSetupDemo/JenkinsSetupDemoApplicationTests.java
@@ -16,9 +16,9 @@ class JenkinsSetupDemoApplicationTests {
 	Logger logger = LoggerFactory.getLogger(JenkinsSetupDemoApplicationTests.class);
 
 	@Test
-	public void contextLoads() {
+	public  contextLoads() {
 		logger.info("Test case execution started.....................");
-		
+
 		Assert.assertEquals(true, true);
 	}
 
