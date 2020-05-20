2841de72229a28d35f262f457493380cc7158ca2
Kailash <kailashsaini7@gmail.com>
2020-05-20 14:03:57 +0530

    revertcommit

--- src/main/java/com/example/JenkinsSetupDemo/JenkinsSetupDemoApplication.java
+++ src/main/java/com/example/JenkinsSetupDemo/JenkinsSetupDemoApplication.java
@@ -21,7 +21,7 @@ public class JenkinsSetupDemoApplication {
 
 
 	public static void main(String[] args) {
-		logger.info("Application executed.......");
+		logger.info("Application executed.......11111111111111");
 		SpringApplication.run(JenkinsSetupDemoApplication.class, args);
 	}
 
